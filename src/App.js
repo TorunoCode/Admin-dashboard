abe368d115a86d9db31afa091c3167325fe0fc17
TorunoCode <thanhlam110230@gmail.com>
2022-09-21 22:57:02 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import TopBar from "./components/topbar/TopBar";
 import Home from './pages/home/Home'
-import './app.css'
+import './App.css'
 import {
   BrowserRouter as Router,
   Routes,
